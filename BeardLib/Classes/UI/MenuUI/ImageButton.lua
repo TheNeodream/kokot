7cc4fd1b7d421e1c1383a26fee0683fb6530b50d
TheNeodream <129326033+TheNeodream@users.noreply.github.com>
2023-03-29 21:09:05 +0200

    Add files via upload

---
+++ BeardLib/Classes/UI/MenuUI/ImageButton.lua
@@ -0,0 +1,59 @@
+BeardLib.Items.ImageButton = BeardLib.Items.ImageButton or class(BeardLib.Items.Item)
+local ImageButton = BeardLib.Items.ImageButton
+ImageButton.type_name = "ImageButton"
+ImageButton.IMG = true
+function ImageButton:InitBasicItem()
+    self.h = self.h or self.w
+    self.panel = self.parent_panel:panel({
+        name = self.name,
+        w = self.w,
+        h = self.h,
+	})
+    self:InitBGs()
+    local w = self.icon_w
+    local h = self.icon_h
+    if self.img_scale then
+        w = w or self.w * self.img_scale
+        h = h or self.h * self.img_scale
+    else
+        w = w or self.w - (self.img_offset[1] * 2)
+        h = h or self.h - (self.img_offset[2] * 2)
+    end
+
+    self.img = self.panel:bitmap({
+        name = "img",
+        texture = self.texture,
+        texture_rect = self.texture_rect,
+        color = self.img_color or self.foreground,
+        w = w,
+        h = h,
+        halign = "center",
+        valign = "center",
+        layer = 5
+    })
+    self.img:set_world_center(self.panel:world_center())
+    self:MakeBorder()
+end
+
+function ImageButton:WorkParams(params)
+    ImageButton.super.WorkParams(self, params)
+    self.img_offset = self.img_offset and self:ConvertOffset(self.img_offset, true) or {0,0}
+    self.img_offset[1] = self.img_offset_x or self.img_offset[1]
+	self.img_offset[2] = self.img_offset_y or self.img_offset[2]
+	self.button_type = true
+end
+
+function ImageButton:DoHighlight(highlight)
+    ImageButton.super.DoHighlight(self, highlight)
+    if self.highlight_image and self.img then
+        if self.animate_colors then
+            play_color(self.img, self:GetForeground(highlight))
+        else
+            self.img:set_color(self:GetForeground(highlight))
+        end
+    end
+end
+
+function ImageButton:SetImage(texture, texture_rect)
+    self.img:set_image(texture, texture_rect)
+end
